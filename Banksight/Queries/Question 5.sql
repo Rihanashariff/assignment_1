ca04ea4705b16bb5b4e0f2d11e103b7fe99c01c3
Rihana1118 <rihanashariff18@gmail.com>
2026-05-04 20:06:37 +0530

    merge completed

--- Banksight/Queries/Question 5.sql
+++
@@ -1,6 +0,0 @@
-# Question 5
-# What is the total transaction volume (sum of amounts) by transaction type?
-
-SELECT txn_type,
-       SUM(amount) AS total_volume
-FROM transactions GROUP BY txn_type;
--- Banksight/Queries/Question 7.sql
+++
@@ -1,5 +0,0 @@
-#Ques 7
-#What is the total number of transactions per transaction type?
-SELECT txn_type,
-      count(*) as total_transactions  #Counts all rows for that transaction type
-FROM transactions GROUP BY txn_type;
